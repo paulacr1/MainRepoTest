85e119407a2e5ed0de044cff1848bb22a3061352
Paula Rosa <paula.rosa@hellofresh.com>
2023-04-14 17:13:42 +0200

    Add new changes to main repo

--- app/src/main/java/com/example/mainrepotest/MainActivity.kt
+++ app/src/main/java/com/example/mainrepotest/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android test")
+                    Greeting("Android test new changes")
                 }
             }
         }
